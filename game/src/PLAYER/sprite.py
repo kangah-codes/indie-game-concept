3cfc12022578cd5fbf79ff8f483c75a1f31c1cb6
unknown <akangah89@gmail.com>
2020-09-05 02:12:10 +0100

    Added more player states and game mechanics

--- game/src/PLAYER/sprite.py
+++ game/src/PLAYER/sprite.py
@@ -27,30 +27,67 @@ class Player(pygame.sprite.Sprite, PhysicsObject):
         self.flip = False
         self.is_crouching = False
         self.is_running = False
+        self.is_running_fast = False
+        self.is_walking = False
+        self.is_sliding = False
+
+        # additional
+        self.can_double_jump = True
+        self.is_double_jumping = False
+        self.jump_count = 0
 
     def update(self, dt):
         self.rect.x, self.rect.y = self.pos.x, self.pos.y
-        self.animation.animate(dt)
         self.animation_rect = self.current_frame.get_rect()
         self.animation_rect.center = self.rect.center
 
         self.current_frame = self.animation.get_current_image()
 
         # check if player rect is leaving vertical bounds
-        if self.pos.y + self.rect.height >= DISPLAY_SIZE[1] and not self.isJumping:
+        if self.rect.y + self.rect.height >= DISPLAY_SIZE[1] and not self.isJumping:
             self.touchingGround = True
             self.pos.y = DISPLAY_SIZE[1] - self.rect.height
+            self.isFalling = False
+
+        if self.rect.y + self.rect.height < DISPLAY_SIZE[1]:
+            self.touchingGround = False
 
         # check if player is moving
         if self.acc.x != 0:
             self.isMoving = True
 
+        # touching ground check
+        if self.touchingGround:
+            if not self.isMoving and not self.is_crouching and not self.is_sliding:
+                self.setState(self.base_state)
+
+            self.is_double_jumping = False
+            self.can_double_jump = True
+            self.jump_count = 0
+
+        if self.jump_count >= 2:
+            self.can_double_jump = False
+
+            self.is_double_jumping = True
 
         self.simulateGravity(dt)
         self.simulateMotion()
         self.handleKeypress()
-
+        self.doAnimations(dt)
         self.updateStates()
+
+    def doAnimations(self, dt):
+        # locking frames to last
+        if self.isJumping or self.is_sliding:
+            if not self.animation.is_last_image():
+                self.animation.animate(dt)
+            else:
+                if self.is_sliding:
+                    self.is_sliding = False
+                    self.isMoving = False
+        else:
+            self.animation.animate(dt)
+
 
     def draw(self, display):
         self.image.fill(WHITE)
@@ -61,39 +98,118 @@ class Player(pygame.sprite.Sprite, PhysicsObject):
         keyPress = pygame.key.get_pressed()
         self.acc = pygame.math.Vector2(0, 800)
 
-        if keyPress[pygame.K_d]:
-            self.isMoving = True
+        if keyPress[pygame.K_d] or keyPress[pygame.K_RIGHT]:
             self.flip = False
 
-        if keyPress[pygame.K_a]:
-            self.isMoving = True
+        if keyPress[pygame.K_a] or keyPress[pygame.K_LEFT]:
             self.flip = True
 
-        if keyPress[pygame.K_a] or keyPress[pygame.K_d]:
-            if self.is_crouching:
+        if (keyPress[pygame.K_a] or keyPress[pygame.K_d]):
+            self.is_running = True
+
+        if keyPress[pygame.K_RIGHT] or keyPress[pygame.K_LEFT]:
+            self.is_walking = True
+
+        if (keyPress[pygame.K_a] or keyPress[pygame.K_d]) or (keyPress[pygame.K_RIGHT] or keyPress[pygame.K_LEFT]):
+            self.isMoving = True
+
+            if self.is_crouching or self.is_walking:
                 self.acc.x = 0.1 if not self.flip else -0.1
             else:
-                self.acc.x = 0.5 if not self.flip else -0.5
+                self.acc.x = 0.3 if not self.flip else -0.3
 
-        if not (keyPress[pygame.K_a] or keyPress[pygame.K_d]):
+        if not (keyPress[pygame.K_RIGHT] or keyPress[pygame.K_LEFT]):
+            self.is_walking = False
+
+        if not (keyPress[pygame.K_a] or keyPress[pygame.K_d] or keyPress[pygame.K_RIGHT] or keyPress[pygame.K_LEFT]):
             self.isMoving = False
+            self.is_running = False
 
-        if keyPress[pygame.K_s]:
+        if keyPress[pygame.K_s]or keyPress[pygame.K_DOWN]:
             self.is_crouching = True
-        else:
+
+        if not (keyPress[pygame.K_s] or keyPress[pygame.K_DOWN]):
             self.is_crouching = False
 
-    def updateStates(self):
-        print(self.isMoving)
-        if self.is_crouching and self.touchingGround and not self.isMoving:
-            self.setState('crouch')
-        elif self.is_crouching and self.isMoving:
-            self.setState('crouch_walk')
 
-        else:
-            self.setState(self.base_state)
+    def updateStates(self):
+        """
+        STATES
+        self.is_crouching
+        self.is_running
+        self.is_running_fast
+        self.is_walking
+        self.is_sliding
+        self.isJumping
+        self.isFalling
+        self.isMoving
+        """
+        # default
+        if not self.is_sliding and not self.is_crouching\
+            and not self.isMoving and not self.is_running\
+            and not self.is_walking and not self.is_running_fast\
+            and not (self.isFalling or self.isJumping): self.setState(self.base_state)
+
+        # player sliding
+        if self.is_sliding and not self.is_crouching\
+            and not self.is_running and not self.is_walking\
+            and not self.is_running_fast and \
+            not (self.isFalling or self.isJumping): self.setState('slide')
+
+        # player running
+        if self.is_running and not self.is_crouching\
+            and not self.is_walking and not self.is_sliding\
+            and not self.is_running_fast and\
+            not self.isFalling and not self.isJumping: self.setState('sprint_slow')
+
+        # jumping
+        if self.isJumping and not self.isFalling:
+            if self.is_double_jumping:
+                self.setState('jump_flip')
+            else:
+                self.setState('jump')
+
+        # falling
+        if self.isFalling and not self.isJumping: self.setState('fall')
+
+        # crouching
+        if self.is_crouching and not self.isMoving\
+            and not self.is_running and not self.is_sliding\
+            and not self.is_walking and not self.is_running_fast\
+            and not (self.isJumping or self.isFalling): self.setState('crouch')
+
+        # crouch walking
+        if self.is_crouching and self.isMoving: self.setState('crouch_walk')
+
+        # is_walking
+        if self.is_walking and not self.isJumping\
+            and not self.is_sliding and not self.is_crouching\
+            and not self.is_running\
+            and not self.is_running_fast: self.setState('walk')
+
+        # crouch walk
+        # if self.is_crouching and self.isMoving\
+        #     and not self.is_running and not self.is_sliding\
+        #     and not self.is_walking and not self.is_running_fast\
+        #     and not (self.isJumping or self.isFalling):
+        #     self.setState('crouch_walk')
 
     def setState(self, state):
         if self.current_state != state:
             self.current_state = state
             self.animation = Animation(player_states.get(self.current_state), 1.0)
+
+    # state functions
+    def slide(self):
+        self.is_sliding = True
+        self.is_moving = True
+        self.setState('slide')
+        self.acc.x = 10 if not self.flip else -10
+
+    # simulate jumping
+    def simulateJump(self):
+        if self.can_double_jump:
+            self.isJumping = True
+            self.touchingGround = False
+            self.vel.y = -400
+            self.jump_count += 1
