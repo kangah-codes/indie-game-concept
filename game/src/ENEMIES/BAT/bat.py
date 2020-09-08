8eabaea379c4156c819fa450cf5c33a499d3ba92
unknown <akangah89@gmail.com>
2020-09-08 03:05:10 +0100

    MOdifications to bat entity

--- game/src/ENEMIES/BAT/bat.py
+++ game/src/ENEMIES/BAT/bat.py
@@ -10,19 +10,20 @@ class Bat(EnemyEntity):
     def __init__(self, base_state, typeOf, x, y):
         EnemyEntity.__init__(self, base_state, typeOf, x, y)
         self.speed = 0.5
-
-    def move_to_player(self, player):
-        dx, dy = player.rect.x - self.rect.x, player.rect.y - self.rect.y
-        dist = math.hypot(dx, dy)
-
-        if dist != 0:
-            dx, dy = dx / dist, dy / dist  # Normalize.
-        # Move along this normalized vector towards the player at current speed.
-        self.pos.x += dx * self.speed
-        self.pos.y += dy * self.speed
-
+        self.can_fly = True
 
     def update(self, dt, player):
-        super(Bat, self).update(dt)
+        super(Bat, self).update(dt, player)
 
-        self.move_to_player(player)
+        # self.move_to_player(player)
+
+    def doAnimations(self, dt):
+        super(Bat, self).doAnimations()
+
+        self.animation.animate(dt)
+
+    def die(self):
+        if self.rect.bottom < DISPLAY_SIZE[1]:
+            self.simulateGravity()
+        else:
+            self.dead = True
