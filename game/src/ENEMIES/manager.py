143f44b03d6a9ab6037c8452ba9fc5c03e666a01
unknown <akangah89@gmail.com>
2020-09-08 03:06:22 +0100

    Modifications to enemy manager

--- game/src/ENEMIES/manager.py
+++ game/src/ENEMIES/manager.py
@@ -8,6 +8,7 @@ from .settings import *
 from .load_sprites import *
 from GLOBAL.physics import *
 from GLOBAL.animation import *
+from GLOBAL.functions import *
 
 class EnemyEntity(PhysicsObject, pygame.sprite.Sprite):
     def __init__(self, base_state, typeOf, x, y):
@@ -18,38 +19,130 @@ class EnemyEntity(PhysicsObject, pygame.sprite.Sprite):
         self.current_state = self.base_state
         self.type = typeOf
         self.dict_object = enemy_animations.get(self.type)
+        self.scale = self.dict_object.get('scale')
 
-        self.animation = Animation(self.dict_object.get(self.base_state), self.dict_object.get('scale'))
-        self.current_frame = self.animation.get_current_image()
-        self.rect = self.current_frame.get_rect()
-        self.mask = pygame.mask.from_surface(self.current_frame)
-        self.image = self.current_frame
+        self.animation = Animation(self.dict_object.get(self.base_state), self.scale)
+        self.image = self.animation.get_current_image()
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
 
         self.health = self.dict_object.get('health')
+        self.damage = self.dict_object.get('damage')
         self.dead = False
+        self.flip = False
 
-    def update(self, dt):
+        self.player_in_sight = False
+        self.player_in_attack_range = False
+        self.is_hostile = True
+        self.is_attacking = False
+        self.is_idle = False if self.player_in_sight else True
+
+        # distance from player
+        self.dx = None
+        self.dy = None
+        self.look_distance = 100
+
+    def update(self, dt, player):
         self.doAnimations(dt)
+
+        # changing image if flip
+        if self.flip:
+            self.image = pygame.transform.flip(self.image, self.flip, False)
 
         self.rect.x, self.rect.y = self.pos.x, self.pos.y
 
-    def doAnimations(self, dt):
-        self.animation.animate(dt)
+        if not self.dead:
+            if self.is_hostile:
+                self.lookForPlayer(player)
 
-        self.current_frame = self.animation.get_current_image()
-        self.rect = self.current_frame.get_rect()
-        self.mask = pygame.mask.from_surface(self.current_frame)
-        self.image = self.current_frame
+            if self.rect.x + self.rect.width < player.rect.x:
+                self.flip = False
+
+            if self.rect.x > player.rect.x + player.rect.width:
+                self.flip = True
+
+        if not self.can_fly:
+            self.simulateGravity(dt)
+
+        # make sure entity doesnt fall over
+        if self.rect.bottom >= DISPLAY_SIZE[1]:
+            self.rect.y = DISPLAY_SIZE[1] - self.rect.height
+            self.isFalling = False
+            self.touchingGround = True
+
+        if self.rect.bottom < DISPLAY_SIZE[1]:
+            self.touchingGround = False
+
+        if self.health <= 0:
+            self.die()
+
+    def doAnimations(self, dt):
+        self.image = self.animation.get_current_image()
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+
+        self.updateStates()
 
     def draw(self, display):
         pygame.draw.rect(display, BLUE, ((self.pos.x+self.rect.width/2)-(self.health*10/2), self.pos.y - 5, self.health*10, 2))
-        display.blit(self.current_frame, (self.pos.x, self.pos.y))
+        display.blit(self.image, (self.pos.x, self.pos.y))
 
     def updateStates(self):
         pass
 
+    def setState(self, state):
+        if self.current_state != state:
+            self.current_state = state
+            self.animation = Animation(self.dict_object.get(self.current_state), self.scale)
+
     def die(self):
-        if self.pos.y + self.rect.height < DISPLAY_SIZE[1]:
-            self.simulateGravity()
+        self.dead = True
+
+    def fly_to_player(self, player):
+        self.dx, self.dy = player.rect.x - self.rect.x, player.rect.centery - self.rect.y
+        dist = math.hypot(self.dx, self.dy)
+
+        if dist != 0:
+            self.dx, self.dy = self.dx / dist, self.dy / dist  # Normalize.
+        # Move along this normalized vector towards the player at current speed.
+        self.pos.x += self.dx * self.speed
+        self.pos.y += self.dy * self.speed
+
+
+    def move_to_player(self, player):
+        self.dx, self.dy = player.rect.x - self.rect.x, player.rect.centery - self.rect.y
+        dist = math.hypot(self.dx, self.dy)
+
+        if dist != 0:
+            self.dx /= dist  # Normalize.
+        # Move along this normalized vector towards the player at current speed.
+        # self.pos.x += self.dx * self.speed
+        self.pos.x += self.dx * self.speed
+
+
+    def lookForPlayer(self, player):
+        self.is_idle = False if self.player_in_sight else True
+
+        if abs(player.rect.x - self.rect.x) <= self.look_distance:
+            if self.can_fly:
+                self.fly_to_player(player)
+            else:
+                self.move_to_player(player)
+            self.player_in_sight = True
+            self.isMoving = True
         else:
-            self.dead = True
+            self.isMoving = False
+            self.player_in_sight = False
+            self.move_random()
+
+    def move_random(self):
+        pass
+
+    def performAction(self):
+        pass
+
+    def die(self):
+        pass
+
+    def attack(self):
+        self.is_attacking = True
