790b87fd33cc4b8cb691ca3865e2abeea9663994
unknown <akangah89@gmail.com>
2020-09-09 00:05:11 +0100

    0

--- game/src/test_run.py
+++ game/src/test_run.py
@@ -9,6 +9,8 @@ testGame = GameManager()
 
 player = Player()
 testGame.enemyEntities.add(Goblin('idle', 'goblin', 100, 50))
-# testGame.enemyEntities.add(Eye('idle', 'eye', 100, 50))
+testGame.enemyEntities.add(Eye('idle', 'eye', 100, 50))
+testGame.enemyEntities.add(Bat('idle', 'bat', 100, 50))
+testGame.enemyEntities.add(Tree('idle', 'tree', 100, 50))
 testGame.player = player
 testGame.mainLoop()
