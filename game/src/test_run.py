786e262dbf8fa194bc23d5b297f6acde3c837f85
unknown <akangah89@gmail.com>
2020-09-08 21:54:54 +0100

    Added more entities

--- game/src/test_run.py
+++ game/src/test_run.py
@@ -2,10 +2,13 @@ from MAIN.game_manager import *
 from PLAYER.sprite import *
 from ENEMIES.BAT.bat import *
 from ENEMIES.TREE.tree import *
+from ENEMIES.EYE.eye import *
+from ENEMIES.GOBLIN.goblin import *
 
 testGame = GameManager()
 
 player = Player()
-testGame.enemyEntities.add(Tree('idle', 'tree', 100, 50))
+testGame.enemyEntities.add(Goblin('idle', 'goblin', 100, 50))
+# testGame.enemyEntities.add(Eye('idle', 'eye', 100, 50))
 testGame.player = player
 testGame.mainLoop()
