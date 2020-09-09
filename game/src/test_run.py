29c6789050c2c6d936f289a17a7623ba7710aca8
unknown <akangah89@gmail.com>
2020-09-10 00:58:17 +0100

    Added slime

--- game/src/test_run.py
+++ game/src/test_run.py
@@ -6,11 +6,12 @@ from ENEMIES.eye import *
 from ENEMIES.goblin import *
 from ENEMIES.mushroom import *
 from ENEMIES.skeleton import *
+from ENEMIES.slime import *
 
 testGame = GameManager()
 
 player = Player()
-testGame.enemyEntities.add(Skeleton('idle', 'skeleton', 100, 50))
+testGame.enemyEntities.add(Slime('idle', 'slime', 100, 50))
 # testGame.enemyEntities.add(Eye('idle', 'eye', 100, 50))
 # testGame.enemyEntities.add(Bat('idle', 'bat', 100, 50))
 # testGame.enemyEntities.add(Tree('idle', 'tree', 100, 50))
