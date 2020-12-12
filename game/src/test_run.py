ad0931138729b23d5b26b228dfdfde1fdf8e053a
kangah-codes <akangah89@gmail.com>
2020-12-12 21:14:23 +0000

    Uncommented enemies

--- game/src/test_run.py
+++ game/src/test_run.py
@@ -11,19 +11,19 @@ from ENTITIES.portal import *
 
 testGame = GameManager()
 
-# testGame.enemyEntities.add(Slime('idle', 'slime', 10, 50))
-# testGame.enemyEntities.add(Eye('idle', 'eye', 30, 50))
-# testGame.enemyEntities.add(Bat('idle', 'bat', 50, 50))
-# testGame.enemyEntities.add(Tree('idle', 'tree', 70, 50))
-# testGame.enemyEntities.add(Goblin('idle', 'goblin', 90, 50))
+testGame.enemyEntities.add(Slime('idle', 'slime', 10, 50))
+testGame.enemyEntities.add(Eye('idle', 'eye', 30, 50))
+testGame.enemyEntities.add(Bat('idle', 'bat', 50, 50))
+testGame.enemyEntities.add(Tree('idle', 'tree', 70, 50))
+testGame.enemyEntities.add(Goblin('idle', 'goblin', 90, 50))
 # testGame.enemyEntities.add(Skeleton('idle', 'skeleton', 110, 50))
-# p1 = pygame.sprite.Group()
+
 #
 # p1.add(Portal(100, 100, None, None))
 #
 # p1.add(Portal(200, 100, None, None))
-p1 = Portal(50, 100)
-p2 = Portal(250, 100)
+p1 = Portal(50, 100, 0)
+p2 = Portal(250, 100, 1)
 
 p1.to = p2
 p2.to = p1
