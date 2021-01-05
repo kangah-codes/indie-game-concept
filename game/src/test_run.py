5bf19a740be9a68a88cbc50660054abbdebd5862
kangah-codes <akangah89@gmail.com>
2021-01-05 14:56:40 +0000

    Comment out entitiy spawn

--- game/src/test_run.py
+++ game/src/test_run.py
@@ -9,13 +9,14 @@ from ENEMIES.skeleton import *
 from ENEMIES.slime import *
 from ENTITIES.portal import *
 
+
 testGame = GameManager()
 
-testGame.enemyEntities.add(Slime('idle', 'slime', 10, 50))
-testGame.enemyEntities.add(Eye('idle', 'eye', 30, 50))
-testGame.enemyEntities.add(Bat('idle', 'bat', 50, 50))
-testGame.enemyEntities.add(Tree('idle', 'tree', 70, 50))
-testGame.enemyEntities.add(Goblin('idle', 'goblin', 90, 50))
+# testGame.enemyEntities.add(Slime('idle', 'slime', 10, 50))
+# testGame.enemyEntities.add(Eye('idle', 'eye', 30, 50))
+# testGame.enemyEntities.add(Bat('idle', 'bat', 50, 50))
+# testGame.enemyEntities.add(Tree('idle', 'tree', 70, 50))
+# testGame.enemyEntities.add(Goblin('idle', 'goblin', 90, 50))
 # testGame.enemyEntities.add(Skeleton('idle', 'skeleton', 110, 50))
 
 #
